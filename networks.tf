ebc154e160a6ba63c0b13bac2e455d12d6b10a54
liner246 <liner246@ukr.net>
2021-05-14 12:17:58 +0000

    validate

--- networks.tf
+++ networks.tf
@@ -1,10 +1,10 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE NETWORK
 # ---------------------------------------------------------------------------------------------------------------------
- 
+
 #Create VPC 
 resource "aws_vpc" "vpc_Jenkins" {
-#  provider             = aws.region-master
+  #  provider             = aws.region-master
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -12,22 +12,22 @@ resource "aws_vpc" "vpc_Jenkins" {
     Name = "vpc-jenkins"
   }
 }
-  
+
 #Create IGW 
 resource "aws_internet_gateway" "igw" {
-#  provider = aws.region-master
-  vpc_id   = aws_vpc.vpc_Jenkins.id
+  #  provider = aws.region-master
+  vpc_id = aws_vpc.vpc_Jenkins.id
 }
 
 #Get all available AZ's in VPC for master region
 data "aws_availability_zones" "azs" {
-#  provider = aws.region-master
-  state    = "available"
+  #  provider = aws.region-master
+  state = "available"
 }
 
 #Create subnet # 1 
 resource "aws_subnet" "subnet_1" {
-#  provider          = aws.region-master
+  #  provider          = aws.region-master
   availability_zone = element(data.aws_availability_zones.azs.names, 0)
   vpc_id            = aws_vpc.vpc_Jenkins.id
   cidr_block        = "10.0.1.0/24"
--- security_groups.tf
+++ security_groups.tf
@@ -3,10 +3,10 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_security_group" "Jenkins_SG" {
-  name = "Jenkins_SG"
-  provider = aws.region-master
+  name        = "Jenkins_SG"
+  provider    = aws.region-master
   description = "Allow TCP/8080, TCP/22, TCP/80, TCP/443 "
-  vpc_id = aws_vpc.vpc_Jenkins.id
+  vpc_id      = aws_vpc.vpc_Jenkins.id
 
   # Inbound 8080 from anywhere
   ingress {
@@ -40,7 +40,7 @@ resource "aws_security_group" "Jenkins_SG" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   # Outbound https from anywhere
   egress {
     from_port   = 0
