6867d46a286d727558fabd3eec7b9abd98a4bfdd
VitaliyPetrenko <liner246@ukr.net>
2021-05-17 16:35:45 +0300

    change1

--- instances.tf
+++ instances.tf
@@ -57,7 +57,7 @@ resource "aws_instance" "jenkins-worker" {
   tags = {
     Name = join("_", ["jenkins_worker_tf", count.index + 1])
   }
-  depends_on = [aws_main_route_table_association.set-master-default-rt-assoc, aws.instance.jenkins-master]
+  depends_on = [aws_main_route_table_association.set-master-default-rt-assoc, aws_instance.jenkins-master]
 }
   
 resource "aws_key_pair" "master-key" {
